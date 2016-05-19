a1ea81f4f9b0dfcfe0fe528da249ea24a5b4ec5f
zhouHaiYang <ocean@>
2016-05-19 14:50:09 +0800

    初始化项目

---
+++ web/web/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  web
+//
+//  Created by Ocean on 16/5/19.
+//  Copyright © 2016年 Ocean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ web/web/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  web
+//
+//  Created by Ocean on 16/5/19.
+//  Copyright © 2016年 Ocean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
